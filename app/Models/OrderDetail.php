7d3c492cad8d240254d1ae211794073889af1221
mrakasondara <rakasondara21@gmail.com>
2026-07-16 12:00:42 +0700

    feat : update order model and add orders details model

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,8 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 #[Fillable(['order_id','quantity','ticket_code', 'ticket_category_id'])]
 class OrderDetail extends Model
 {
+    protected $table = 'orders_details';
+    
     public function order()
     {
         $this->belongsTo(Order::class);
     }
+
+    public function ticketCategory()
+    {
+        $this->belongsTo(TicketCategory::class);
+    }
 }
+
---
+++ database/migrations/2026_07_16_031305_create_order_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->integer('quantity');
+            $table->text('ticket_code')->unique();
+            $table->foreignId('ticket_category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders_details');
+    }
+};
